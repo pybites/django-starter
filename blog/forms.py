0e24b91fb70a6e20e439f01d0237b2ac7a5f27d5
Bob Belderbos <bobbelderbos@gmail.com>
2020-02-21 08:36:52 +0100

    add form to model

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from .models import Blog
+
+
+class BlogForm(ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'post', 'slug', 'cover']
