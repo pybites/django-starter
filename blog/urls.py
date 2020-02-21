532f822946b758aa01133f4dcddd4532a2e225d2
Bob Belderbos <bobbelderbos@gmail.com>
2020-02-21 09:07:56 +0100

    forgot to rename methods in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:pk>', views.post_detail, name='post_detail'),
-    path('new', views.post_new, name='post_new'),
-    path('edit/<int:pk>', views.post_edit, name='post_edit'),
-    path('delete/<int:pk>', views.post_delete, name='post_delete'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:pk>', views.blog_detail, name='blog_detail'),
+    path('new', views.blog_new, name='blog_new'),
+    path('edit/<int:pk>', views.blog_edit, name='blog_edit'),
+    path('delete/<int:pk>', views.blog_delete, name='blog_delete'),
 ]
