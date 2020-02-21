1f9e65202931a0c7639d77eb4ad8bb5c32348284
Bob Belderbos <bobbelderbos@gmail.com>
2020-02-21 08:36:17 +0100

    define model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    slug = models.SlugField(max_length=50)
+    cover = models.URLField(blank=True, null=True)
+    added = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.slug
+
+    class Meta:
+        ordering = ['-added']
