c4e366294d35d9caff7c03901aa3376adf16b42d
Bob Belderbos <bobbelderbos@gmail.com>
2020-02-21 09:09:38 +0100

    add views

--- blog/views.py
+++ blog/views.py
@@ -6,20 +6,42 @@ from .forms import BlogForm
 
 
 def blog_list(request):
-    pass
+    posts = Blog.objects.all()
+    return render(request, 'blog/list.html', {'posts': posts})
 
 
-def blog_detail(request):
-    pass
+def blog_detail(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def blog_new(request):
-    pass
+    form = BlogForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Added post')
+        return redirect('blog:list')
+    return render(request, 'blog/form.html', {'form': form})
 
 
-def blog_edit(request):
-    pass
+def blog_edit(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    form = BlogForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Updated post')
+        return redirect('blog:list')
+
+    return render(request, 'blog/form.html', {'post': post,
+                                              'form': form})
 
 
-def blog_delete(request):
-    pass
+def blog_delete(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, 'Deleted post')
+        return redirect('blog:list')
+
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
