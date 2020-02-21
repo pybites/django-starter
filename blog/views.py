99e501dd122d1e746f834ae223c2eb11c59d1b7d
Bob Belderbos <bobbelderbos@gmail.com>
2020-02-21 08:37:01 +0100

    view stubs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
 
-# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
+
+def post_list(request):
+    pass
+
+
+def post_detail(request):
+    pass
+
+
+def post_new(request):
+    pass
+
+
+def post_edit(request):
+    pass
+
+
+def post_delete(request):
+    pass
